e73846d492903edea323710ae84e46e28f33b39d
Dimitar Dimitrov <mitkodimitar@gmail.com>
2025-05-24 19:44:42 -0400

    create rule dataclass

--- recurrent_tasks/__main__.py
+++ recurrent_tasks/__main__.py
@@ -2,6 +2,7 @@
 
 import argparse
 import json
+from dataclasses import dataclass
 from datetime import date
 from datetime import datetime
 from datetime import time
@@ -12,38 +13,54 @@ from pathlib import Path
 import requests
 
 
-def get_cards(
-    defs: list[tuple[str, int | None, int | None, int | None, int | None, int | None]],
-    target_date: date,
-) -> list[tuple[str, date | None]]:
-    """Get cards from definitions"""
+@dataclass
+class Rule:
+    """A rule for creating a recurrent task"""
 
-    cards: list[tuple[str, date | None]] = []
-    for name, year, month, day, weekday, due in defs:
-        if year is not None and year != target_date.year:
+    card_name: str
+    year: int | None
+    month: int | None
+    day: int | None
+    weekday: int | None
+    due_in: int | None
+
+
+def get_cards(
+    rules: list[Rule], evaluation_date: date, *, due_time: time = time(9, 0, 0)
+) -> list[tuple[str, datetime | None]]:
+    """Get cards from rules at an evaluation date"""
+
+    cards: list[tuple[str, datetime | None]] = []
+    for rule in rules:
+        if rule.year is not None and rule.year != evaluation_date.year:
             continue
-        if month is not None and month != target_date.month:
+        if rule.month is not None and rule.month != evaluation_date.month:
             continue
-        if day is not None and day != target_date.day:
+        if rule.day is not None and rule.day != evaluation_date.day:
             continue
-        if weekday is not None and weekday != target_date.weekday():
+        if rule.weekday is not None and rule.weekday != evaluation_date.weekday():
             continue
-        if due is None:
-            cards.append((name, None))
-        else:
-            cards.append((name, target_date + timedelta(days=due)))
+
+        card_due = (
+            None
+            if rule.due_in is None
+            else datetime.combine(
+                evaluation_date + timedelta(days=rule.due_in), due_time
+            )
+        )
+
+        cards.append((rule.card_name, card_due))
 
     return cards
 
 
 def create_cards(
-    cards: list[tuple[str, date | None]],
+    cards: list[tuple[str, datetime | None]],
     list_id: str,
     api_key: str,
     token: str,
     *,
     timeout: int = 60,
-    due_time: time = time(9, 0, 0),
 ) -> None:
     """Create cards from a list if they don't already exist"""
 
@@ -65,11 +82,7 @@ def create_cards(
             "token": token,
         }
         if card_due is not None:
-            query["due"] = (
-                datetime.combine(card_due, due_time)
-                .astimezone(timezone.utc)
-                .isoformat()
-            )
+            query["due"] = card_due.astimezone(timezone.utc).isoformat()
         response = requests.post(url, params=query, timeout=timeout)
         if response.status_code != 200:
             raise RuntimeError(f"Couldn't create card {card_name}")
@@ -81,13 +94,13 @@ def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
 
     parser.add_argument(
-        "defs_filepath", type=Path, help="Path to a JSON file containing desired defs"
+        "rules_filepath", type=Path, help="Path to a JSON file containing rules"
     )
-    parser.add_argument("list_id", type=str, help="ID for target list")
+    parser.add_argument("list_id", type=str, help="ID of target list")
     parser.add_argument(
         "secrets_filepath",
         type=Path,
-        help="Path to a JSON file containing an API key aand a token",
+        help="Path to a JSON file containing an API key and a token",
     )
 
     return parser.parse_args()
@@ -98,15 +111,15 @@ def main() -> None:
 
     args = parse_args()
 
-    with args.defs_filepath.open(encoding="UTF-8") as defs_file:
-        defs = json.load(defs_file)
+    with args.rules_filepath.open(encoding="UTF-8") as rules_file:
+        rules = [Rule(**rule) for rule in json.load(rules_file)]
     list_id = args.list_id
     with args.secrets_filepath.open(encoding="UTF-8") as secrets_file:
         secrets = json.load(secrets_file)
         api_key = secrets["api_key"]
         token = secrets["token"]
 
-    cards = get_cards(defs, date.today())
+    cards = get_cards(rules, date.today())
     create_cards(cards, list_id, api_key, token)
 
 
